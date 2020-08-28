01d30a38a7d97c0f9c78459fe733d68d439006aa
JakesDesktop <omgacat520@gmail.com>
2020-08-27 21:42:16 -0600

    added questions.txt

--- VariableOutputForms/Form1.Designer.cs
+++ VariableOutputForms/Form1.Designer.cs
@@ -28,13 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblComparedValue = new System.Windows.Forms.Label();
+            this.lblActualValue = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblComparedValue
+            // 
+            this.lblComparedValue.AutoSize = true;
+            this.lblComparedValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComparedValue.Location = new System.Drawing.Point(12, 9);
+            this.lblComparedValue.Name = "lblComparedValue";
+            this.lblComparedValue.Size = new System.Drawing.Size(453, 73);
+            this.lblComparedValue.TabIndex = 0;
+            this.lblComparedValue.Text = "Original Value:";
+            // 
+            // lblActualValue
+            // 
+            this.lblActualValue.AutoSize = true;
+            this.lblActualValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblActualValue.Location = new System.Drawing.Point(12, 133);
+            this.lblActualValue.Name = "lblActualValue";
+            this.lblActualValue.Size = new System.Drawing.Size(411, 73);
+            this.lblActualValue.TabIndex = 1;
+            this.lblActualValue.Text = "Actual Value:";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Location = new System.Drawing.Point(25, 295);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(440, 143);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(793, 450);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lblActualValue);
+            this.Controls.Add(this.lblComparedValue);
+            this.Name = "Form1";
+            this.Text = "Project Beta";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblComparedValue;
+        private System.Windows.Forms.Label lblActualValue;
+        private System.Windows.Forms.Button btnOK;
     }
 }
 
--- VariableOutputForms/Form1.cs
+++ VariableOutputForms/Form1.cs
@@ -14,5 +14,26 @@ namespace VariableOutputForms
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e) //onload, execute this stuff
+        {
+            double comparedValue, actualValue;//values to compare
+            var objVariables = new Okuma.CMDATAPI.DataAPI.CVariables(); //object used to retrieve variables from API
+
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized; //hide the form when it's created, no need to see it until the variables are different. This is the "minimize" effect. Change the last part of it to Maximized when needing to be maximized.
+
+            lblComparedValue.Visible = false; //these values should be hidden until the user needs them.
+            lblActualValue.Visible = true;
+
+            do
+            {
+                objVariables.GetCommonVariableValue(195);
+            } while (comparedValue != 0 || Math.Abs(comparedValue) > 0.01);
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+        }
     }
 }
